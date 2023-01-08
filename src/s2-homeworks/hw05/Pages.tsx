40885dc6d6960c526feadfa8589bc7c74401fd5b
Aleksei Gazov <lekha.gazov@yandex.ru>
2023-01-08 21:32:23 +0300

    hw5 refactor

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -19,6 +19,8 @@ function Pages() {
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
+                <Route path={'/'} element={ <Navigate to={PATH.PRE_JUNIOR}/>}/>
+
                 <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 <Route path={PATH.JUNIOR} element={<Junior/>}/>
                 <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
